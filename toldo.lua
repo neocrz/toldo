d6482b1d6d7f6936caf6c5a0efcb4ad052c00f14
neocrz <fernandogomescruz@gmail.com>
2023-09-14 17:38:21 -0300

    Added: edit option

--- toldo.lua
+++ toldo.lua
@@ -121,43 +121,71 @@ local function check_todo(idx, _)
 	end
 	db:save()
 end
+local function edit_todo(idx, _, content)
+	local tb = _ or DEFAULT_CT
+	local index = tonumber(idx)
 
-local add_cmd = parser:command("add"):summary("Add a todo.")
-add_cmd:option("-c --category", "To specify the category, without it, todos will be added to the default category.")
-add_cmd:argument("text", 'the todo text. between " "'):args(1)
+	if category_exists(tb) then
+		local todo = db.todos[tb][index]
+		if todo then
+			todo.content = content or ""
+		else
+			print("The index '" .. idx .. "' does not exist in this category.")
+		end
+	else
+		print("Category '" .. tb .. "' does not exists.")
+	end
 
-local list_cmd = parser:command("list"):summary("Lists all the todos from a category.")
+	db:save()
+end
+---[====[ cli commands
+local add_cmd = parser:command("add"):summary("Add a task.")
+add_cmd:option("-c --category", "To specify the category, without it, task will be added to the default category.")
+add_cmd:argument("text", 'The task text. between " "'):args(1)
+
+local list_cmd = parser:command("list"):summary("Lists all the tasks from a category.")
 list_cmd:option("-c --category", "To specify another category instead of the default one.")
 
-local rm_cmd = parser:command("rm"):summary("Remove a todo.")
+local rm_cmd = parser:command("rm"):summary("Remove a task.")
 rm_cmd:option(
 	"-c --category",
-	"To specify the category, without it, the todo from the default category with the index will be removed."
+	"To specify the category. Without it, the task from the default category with the index will be removed."
 )
-rm_cmd:argument("index", "index of the todo (use toldo list to get the todos indexes).")
+rm_cmd:argument("index", "Index of the task (use toldo list to get the task indexes).")
 
-local check_cmd = parser:command("check"):summary("Check or uncheck a todo.")
+local check_cmd = parser:command("check"):summary("Check or uncheck a task.")
 check_cmd:option("-c --category", "to specify a category instead of the default one.")
-check_cmd:argument("index", "index of the todo (use toldo list to get the todo's indexes).")
+check_cmd:argument("index", "index of the task (use toldo list to get the task's indexes).")
 
+local edit_cmd = parser:command("edit"):summary("Edit a task.")
+edit_cmd:option("-c --category", "to specify a category instead of the default one.")
+edit_cmd:argument("index", "Index of the task (use toldo list to get the task's indexes).")
+edit_cmd:argument("content", "The new content of the task.")
+--]====]
+
+-- ARGS variable
 local args = parser:parse()
+
+local function print_no_category()
+	print("Category does not exists")
+end
 
 if args.add then
 	if args.category then
 		if not category_exists(args.category) then
-			print("Category does not exists")
+			print_no_category()
 			return false
 		end
 	end
 	if not args.text then
-		print("Please provide a todo text")
+		print("Please provide a task text")
 		return false
 	end
 	add_todo(args.text, args.category)
 elseif args.list then
 	if args.category then
 		if not category_exists(args.category) then
-			print("Category does not exists")
+			print_no_category()
 			return false
 		end
 	end
@@ -166,7 +194,7 @@ elseif args.list then
 elseif args.rm then
 	if args.category then
 		if not category_exists(args.category) then
-			print("Category does not exists")
+			print_no_category()
 			return false
 		end
 	end
@@ -180,7 +208,7 @@ elseif args.rm then
 elseif args.check then
 	if args.category then
 		if not category_exists(args.category) then
-			print("Category does not exists")
+			print_no_category()
 			return false
 		end
 	end
@@ -192,4 +220,18 @@ elseif args.check then
 	end
 
 	check_todo(index, args.category)
+elseif args.edit then
+	if args.category then
+		if not category_exists(args.category) then
+			print_no_category()
+			return false
+		end
+	end
+
+	local index = tonumber(args.index)
+	if not index then
+		print("Please give me a valid number for the todo index.")
+		return false
+	end
+	edit_todo(args.index, args.category, args.content)
 end
