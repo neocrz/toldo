a6ac924ad84584edccb739fc856a91c77482d04f
neocrz <fernandogomescruz@gmail.com>
2023-09-16 00:00:01 -0300

    rm: 2lines of unutilized code

--- lib/models/init.lua
+++ lib/models/init.lua
@@ -2,9 +2,6 @@ local Class = require("classic")
 
 
 
-local Task = Class:extend()
-local Category = Class:extend()
-
 -- OBS. if is init (this will be the folder name) else it will be the module file
 local pathOfThisMod = ...                           -- pathOfThisMod is now 'lib.foo.bar'
 local folderOfThisFile = (...):match("(.-)[^%.]+$") -- returns 'lib.foo.'
